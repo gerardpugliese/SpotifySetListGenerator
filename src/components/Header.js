a9d532531b939b2479a4437d5a9d3f69a8b75aac
Gerard Pugliese <pugliesegerard@gmail.com>
2023-04-26 12:22:09 -0400

    Adds code comments.

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,50 +6,57 @@ function Header(props) {
     const [userId, setUserId] = useState(null)
 
     useEffect(() => {
-    const hash = window.location.hash
-    let token = window.localStorage.getItem("token")
-    
-    if (!token && hash) {
-        token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+      //Retrieve token from local storage.
+      const hash = window.location.hash
+      let token = window.localStorage.getItem("token")
+      
+      if (!token && hash) {
+          token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
 
-        window.location.hash = ""
-        window.localStorage.setItem("token", token)
-    }
-    setToken(token)
-    if (userId === null && token !== null) {
-      //Get user profile
-      fetch(`https://api.spotify.com/v1/me`, {
-          method: "GET",
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
+          window.location.hash = ""
+          window.localStorage.setItem("token", token)
+      }
+
+      setToken(token)
+
+      //If we have a token but haven't retried the user's Spotify id, get it.
+      if (userId === null && token !== null) {
+        //Get user profile
+        fetch(`https://api.spotify.com/v1/me`, {
+            method: "GET",
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`
+            }
+        })
+        .then(resp => resp.json())
+        .then(resp => {
+          if ("error" in resp) {
+            logout()
+          } else {
+            setUserId(resp.id)
+            if (props.propagateUserId) {
+              props.propagateUserId(resp.id)
+            }
           }
-      })
-      .then(resp => resp.json())
-      .then(resp => {
-        if ("error" in resp) {
-          logout()
-        } else {
-          setUserId(resp.id)
-          if (props.propagateUserId) {
-            props.propagateUserId(resp.id)
-          }
-        }
-      })
-      .catch(error => {
-        console.log(error)
-      })
-    }
+        })
+        .catch(error => {
+          console.log(error)
+        })
+      }
     }, [])
 
+    
     const login = () => {
+      //This function opens up a window that allows the user to log into their spotify account.
       window.location.href = `${process.env.REACT_APP_AUTH_ENDPOINT}?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=${process.env.REACT_APP_RESPONSE_TYPE}&scope=${process.env.REACT_APP_SCOPE}`
     }
 
     const logout = () => {
-        setToken("")
-        window.localStorage.removeItem("token")
+      //This function logouts the user from Spotify.
+      setToken("")
+      window.localStorage.removeItem("token")
     } 
     
     return(
