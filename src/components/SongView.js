78a549aa300e52c80abea831c0d0fa28f4db091f
Gerard Pugliese <pugliesegerard@gmail.com>
2023-05-10 12:26:48 -0400

    Changes format of code comments.

--- src/components/SongView.js
+++ src/components/SongView.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 function SongView (props) {
+    /**
+     * Song view in setlist view.
+     */
     return (
         <div onClick={(e) => {
             e.stopPropagation()
